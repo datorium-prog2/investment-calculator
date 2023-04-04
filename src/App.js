1d6c6ba59efb58210065bed3d81c31fff10d3f33
Serzsh <serzants.janis@gmail.com>
2023-04-04 13:23:14 +0300

    Added calculations

--- src/App.js
+++ src/App.js
@@ -1,31 +1,100 @@
-import './App.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { sp500rates } from './data/sp500rates'
+import { useState } from 'react';
 
 function App() {
+  const [fromData, setFormData] = useState({
+    amount: 100,
+    years: 10,
+  })
 
+  const [result, setResult] = useState('')
+
+  const calculationHandler = () => {
+    const historyPercentage = sp500rates.filter((rate, index) => {
+        return index < fromData.years
+    })
+
+    let amount = fromData.amount
+
+    historyPercentage.forEach((rate) => {
+      amount = amount + (amount * (rate / 100))
+    })
+
+    setResult(`
+      Ja tu būtu ieguldījis S&P 500 fondā ${fromData.amount}€ pirms ${fromData.years} gadiem,
+      tad tagad tu būtu ${(amount - fromData.amount).toFixed(2)}€ plusā!
+    `)
+
+    console.log(amount.toFixed(2));
+  }
 
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+    <Container className="mt-3">
+      <Row>
+        <Col>
+          <h1 className='text-center'>Investīciju vēsturiskais aprēķins</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Form onSubmit={(e) => {
+            e.preventDefault()
+            calculationHandler()
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+          }}>
+            <Form.Group className="mb-3" controlId="amount">
+              <Form.Label>Amount in €</Form.Label>
+              <Form.Control 
+                type="number" 
+                placeholder="100 €"
+                value={fromData.amount}
+                onChange={(e) => {
+                  setFormData({
+                    ...fromData,
+                     amount: e.target.value,
+                  })
+                }}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="years">
+              <Form.Label>Pirms cik gadiem veici ieguldījumu</Form.Label>
+              <Form.Select
+                value={fromData.years}
+                onChange={(e) => {
+                  setFormData({
+                    ...fromData,
+                     years: e.target.value,
+                  })
+                }}
+              >
+                  <option value="5">5</option>
+                  <option value="10">10</option>
+                  <option value="15">15</option>
+                  <option value="25">25</option>
+                  <option value="30">30</option>
+                  <option value="35">35</option>
+                  <option value="40">40</option>
+              </Form.Select>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Izrēķināt
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+      <Row className="mt-4">
+        <Col>
+            <h4>
+              {result}
+            </h4>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
